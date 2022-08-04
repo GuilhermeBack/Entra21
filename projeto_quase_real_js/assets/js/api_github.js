21532a3d0a9312d5f2bc1268fcf1db0adcca639b
guilherme <guilhermeluiz_back@hotmail.com>
2022-08-03 22:24:55 -0300

    cont2

--- projeto_quase_real_js/assets/js/api_github.js
+++ projeto_quase_real_js/assets/js/api_github.js
@@ -2,9 +2,17 @@ function renderiza_foto(foto) {
     const img_foto = document.getElementById('foto')
     img_foto.src = `${foto}`
 }
-fetch("https://api.github.com/users/gabrielback")
+const headers = new Headers();
+headers.append ('Authorization', 'token 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');
+
+fetch("https://api.github.com/users/guilhermeback",
+{
+    headers: headers
+}
+)
 .then(response => response.json())
 .then(data => {
+    console.log(data)
     renderiza_foto(data.avatar_url)
     document.getEelementById('nome').innerText = data.name
 })
